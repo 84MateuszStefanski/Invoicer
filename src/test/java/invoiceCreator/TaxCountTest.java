7c4a992a78c1cda5af64329afec71bc9b54b762c
84MateuszStefanski <84mateuszstefanski@gmail.com>
2020-12-02 20:21:45 +0100

    "Testy kolejna faza, działa"

--- src/test/java/invoiceCreator/TaxCountTest.java
+++ src/test/java/invoiceCreator/TaxCountTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class TaxCountTest {
 
     @Test
-    void shouldcalculateTax() {
+    void shouldCalculateTax() {
         //given
         Product product = new Product("Nazwa", "Nr katalogowy", new BigDecimal("100"),new BigDecimal("5"));
         BigDecimal tax = new BigDecimal(0.23);
@@ -26,9 +26,24 @@ class TaxCountTest {
         assertThat(result).isEqualTo(new BigDecimal("23.0000000000000009992007221626408863812685012817382812500"));
        // assertThat(result).isCloseTo(new BigDecimal("23.00"),new BigDecimal("0.1"));
        // assertEquals(new BigDecimal("23").stripTrailingZeros(), result.stripTrailingZeros());
+        assertEquals(23 , result.intValue());
     }
 
     @Test
-    void shouldcalculateGrossPrice() {
+    void shouldCalculateGrossPrice() {
+
+        //given
+        Product product = new Product("Nazwa", "Nr katalogowy", new BigDecimal("100"),new BigDecimal("5"));
+        BigDecimal tax = new BigDecimal(0.23);
+        TaxCount taxCount = new TaxCount();
+
+        //when
+        BigDecimal result = taxCount.calculateGrossPrice(product);
+
+        //then
+        assertThat(result).isEqualTo(new BigDecimal("123.0000000000000009992007221626408863812685012817382812500"));
+        assertEquals(123 , result.intValue());
+
+
     }
 }
